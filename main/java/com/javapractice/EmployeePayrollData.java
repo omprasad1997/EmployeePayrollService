253a2848996eadf9fa72954fb6f79de13b2b2def
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-08 00:01:11 +0530

    Welcome message

---
+++ main/java/com/javapractice/EmployeePayrollData.java
@@ -0,0 +1,7 @@
+package com.javapractice;
+
+public class EmployeePayrollData {
+    public void welcome(){
+        System.out.println("Welcome");
+    }
+}
---
+++ test/java/com/javapractice/EmployeePayrollServiceTest.java
@@ -0,0 +1,7 @@
+package com.javapractice;
+
+public class EmployeePayrollServiceTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
