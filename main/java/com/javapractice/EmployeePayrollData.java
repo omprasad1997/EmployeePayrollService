c877a1f2cb15f73d1baf22a738700b02e74c386e
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-08 00:20:19 +0530

    Updated : Create an Employee Payroll Service to read and write Employee Payroll to console

--- main/java/com/javapractice/EmployeePayrollData.java
+++ main/java/com/javapractice/EmployeePayrollData.java
@@ -1,7 +1,46 @@
 package com.javapractice;
 
 public class EmployeePayrollData {
-    public void welcome(){
-        System.out.println("Welcome");
+    private int id;
+    private String name;
+    private double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayroll{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
     }
 }
