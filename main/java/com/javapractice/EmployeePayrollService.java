fe1e0d5dd7a3ac802e1096b3583915465ea99d0a
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-08 23:07:16 +0530

    Updated : Wrote a program to demonstrate file operations like check file exists,delete ,create file etc

--- main/java/com/javapractice/EmployeePayrollService.java
+++ main/java/com/javapractice/EmployeePayrollService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+    public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
 
     public EmployeePayrollService(){}
