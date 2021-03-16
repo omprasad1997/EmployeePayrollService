9c2b270c96b97d1b9b50bee330370931f0e69a75
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-16 18:26:40 +0530

    Updated : update the salary for employee and sync it with database using JDBC prepared statement

--- main/java/com/javapractice/EmployeePayrollData.java
+++ main/java/com/javapractice/EmployeePayrollData.java
@@ -1,14 +1,23 @@
 package com.javapractice;
 
-public class EmployeePayrollData {
-    private int id;
-    private String name;
-    private double salary;
+import java.time.LocalDate;
+import java.util.Objects;
 
-    public EmployeePayrollData(int id, String name, double salary) {
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public double salary;
+    public LocalDate startDate;
+
+    public EmployeePayrollData(Integer id, String name, double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+    }
+
+    public EmployeePayrollData(int id, String name, double salary, LocalDate startDate) {
+        this(id,name,salary);
+        this.startDate = startDate;
     }
 
     public int getId() {
@@ -37,10 +46,20 @@ public class EmployeePayrollData {
 
     @Override
     public String toString() {
-        return "EmployeePayroll{" +
+        return "EmployeePayrollData{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", salary=" + salary +
+                ", startDate=" + startDate +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EmployeePayrollData that = (EmployeePayrollData) o;
+        return id == that.id && Double.compare(that.salary, salary) == 0 &&
+                name.equals(that.name);
     }
 }
--- main/java/com/javapractice/EmployeePayrollService.java
+++ main/java/com/javapractice/EmployeePayrollService.java
@@ -5,13 +5,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
+
+
+
     public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO}
     private List<EmployeePayrollData> employeePayrollList;
+    private EmployeePayrollDBService employeePayrollDBService;
 
-    public EmployeePayrollService(){}
+    public EmployeePayrollService(){
+         employeePayrollDBService = EmployeePayrollDBService.getInstance();
+    }
 
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+
+        this();
         this.employeePayrollList = employeePayrollList;
+
     }
 
     public static void main(String[] args) {
@@ -31,6 +40,32 @@ public class EmployeePayrollService {
         double salary = consoleInputReader.nextDouble();
         employeePayrollList.add(new EmployeePayrollData(id,name,salary));
     }
+
+    public List<EmployeePayrollData> readEmployeePayrollDataFromDB(IOService ioService) {
+            if(ioService.equals(IOService.DB_IO))
+                this.employeePayrollList = employeePayrollDBService.readData();
+            return this.employeePayrollList;
+    }
+
+    public boolean checkEmployeePayrollInSyncWithDB(String name) {
+        List<EmployeePayrollData> employeePayrollDataList = employeePayrollDBService.getEmployeePayrollData(name);
+        return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
+    }
+
+    public void updateEmployeeSalary(String name, double salary) {
+            int result  = employeePayrollDBService.updateEmployeeData(name,salary);
+            if(result == 0) return;
+            EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+            if(employeePayrollData!=null) employeePayrollData.salary= salary;  //p1
+    }
+
+    private EmployeePayrollData getEmployeePayrollData(String name) {
+        return this.employeePayrollList.stream()
+                .filter(employeeDataItem -> employeeDataItem.name.equals(name))  //p2 = getName() = name
+                .findFirst()
+                .orElse(null);
+    }
+
     public void writeEmployeePayrollData(EmployeePayrollService.IOService ioService){
         if(ioService.equals(IOService.CONSOLE_IO))
              System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
